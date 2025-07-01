d157c5d1ef41998827df2b52a7c4b9a9c2385bdf
Arpita Mishra <arpitakumari478@gmail.com>
2025-07-01 23:53:22 +0530

    utils are discussed

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB CONNECTION FAILED !!!",error)
+
+})
+
 // import express from "express";
 // const app=express()
 // (async ()=>{
